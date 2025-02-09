6709c1d018431db3c6be48be59547315869ba14f
Kuba62 <Jakubnorton@gmail.com>
2025-02-09 17:20:41 +0100

    added icon and page title

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PPYWYH3T.mjs";import"./chunk-SG7FVOXY.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-IMRXIQR6.mjs";import"./chunk-SG7FVOXY.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
